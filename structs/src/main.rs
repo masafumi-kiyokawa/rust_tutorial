e7f09f27e1bea00832478f1466d2dca303e3865d
fujitsubo <fumikiyo717@gmail.com>
2025-08-31 22:57:18 +0900

    Finish chapter5.1

---
+++ structs/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremial@example.com");
+
+    let user2 = User {
+        ..user1
+    };
+
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        // email: email,
+        // username: username,
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
