c84c043abfbaa29c87ae07a97bb236201d784d92
fujitsubo <fumikiyo717@gmail.com>
2025-09-11 23:30:32 +0900

    Finish chapter10

--- generic_trait_lifetime/src/main.rs
+++ generic_trait_lifetime/src/main.rs
@@ -51,6 +51,19 @@ fn main() {
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
 
 fn derive_max(list: &[i32]) -> i32 {
@@ -237,3 +250,59 @@ impl<T: Display + PartialOrd> Pair<T> {
 // impl<T: Display> ToString for T {
 
 // }
+
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn longest<'a, 'b>(x: &'a str, y: &'b str) -> &'a str {
+//     x
+// }
+fn longest<'a, 'b>(x: &'a str, y: &str) -> &'a str {
+    x
+}
+// fn longest<'a>(x: &str, y: &str) -> &'a str {
+//     let result = String::from("realy long string");
+//     result.as_str()  // これは関数外ではもはや有効ではないのでコンパイルエラー
+// }
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}  // 構造体のライフタイムはpartと同じ
+
+// ライフタイムの省略規則
+// 1. 引数はそれぞれ独自のライフタイム引数を得る
+// e.g. fn foo(a: &str, b: &i32) は fn foo<'a, 'b>(a: &'a str, b: &'b i32) のように解釈される
+// 2. 入力ライフタイム引数が1つ(1種類)だけなら、そのライフタイムが全ての出力ライフタイム引数に代入される
+// e.g. fn foo<'a>(a: &'a str) -> &str は fn foo<'a>(a: &'a str) -> &'a str として解釈される
+// 3. &selfや&mut selfが入力ライフタイム引数にあるなら、selfのライフタイムが全ての出力ライフタイム引数に代入される
+// e.g. impl<'a> ImportantExcerpt<'a> { fn foo(&self) -> &str {} } は impl<'a> ImportantExcerpt<'a> { fn foo(&self) -> &'a str {} }として解釈される
+
+// 静的ライフタイム'static
+// let s: &'static str = "I have a static lifetime";
+
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    //       "アナウンス！ {}"
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+// annは戻り値に関係ないのでライフタイム指定は不要
+// TはDisplayトレイトを実装する型ならなんでもOK
+// ライフタイムは一種のジェネリックなので<>内に収まっている
